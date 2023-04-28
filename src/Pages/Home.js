2d9d84a4ea35de0568b5a95818243496967239c1
KrishnaKV-7 <krishnakumarvedagiri07@gmail.com>
2023-04-27 20:01:29 -0500

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Box } from '@mui/material';
 
 import Exercises from '../Components/Exercises';
-import SearchExercises from '../Components/SearchExercises';
+
 import HeroBanner from '../Components/HeroBanner';
 
 const Home = () => {
-  const [exercises, setExercises] = useState([]);
-  const [bodyPart, setBodyPart] = useState('all');
-
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+      
+      <Exercises />
     </Box>
-  );
-};
+  )
+}
 
 export default Home;
