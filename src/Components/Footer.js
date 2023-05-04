924c957747e4085a9ece8ba4a8e6ca0a18c96907
KrishnaKV-7 <krishnakumarvedagiri07@gmail.com>
2023-05-04 09:49:41 -0500

    Update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,8 @@ import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    <Stack gap="80px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '300px', height: '61px' }} />
     </Stack>
     <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Fitness Club</Typography>
   </Box>
